2af932a8c22697fbfe3139594ca7a16ff38e3fe0
Omri Bahumi <omrib@everything.me>
2013-08-06 23:26:21 +0300

    Initial commit

---
+++ static/js/main.js
@@ -0,0 +1,56 @@
+function $(selector) {
+    return document.querySelector(selector);
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    var GroovyClient = function(address) {
+        var self = this;
+        self.ws = new WebSocket(address);
+
+        self.send = function send(data) {
+            self.ws.send(JSON.stringify(data));
+        }
+
+        self.call = function invoke(action, args) {
+            var data = {
+                'type': 'call',
+                'call': {
+                    'function': action
+                }
+            };
+
+            if (typeof args != undefined) {
+                data['args'] = args;
+            }
+
+            self.send(data);
+        };
+
+        self.ws.onopen = function(){
+            console.log("WebSocket connected");
+        };
+
+        self.ws.onmessage = function(message){
+            data = JSON.parse(message.data);
+            console.log(data);
+        };
+    }
+
+    client = new GroovyClient("ws://" + window.location.host + "/ws/client/dummyClientId");
+
+    $("#prev").addEventListener('click', function(){
+        client.call("prev");
+    });
+
+    $("#play").addEventListener('click', function(){
+        client.call("play");
+    });
+
+    $("#pause").addEventListener('click', function(){
+        client.call("pause");
+    });
+
+    $("#next").addEventListener('click', function(){
+        client.call("next");
+    });
+});
